226fc163919ad52976a7a226b20208a6d7c472bb
yifang <samantha12dong@gmail.com>
2018-05-08 23:07:35 +0200

    solution of Polymino challenge

---
+++ src/main/java/Polyomino.java
@@ -0,0 +1,137 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by yifang on 5/8/2018.
+ */
+public class Polyomino {
+
+    private List<Position> squares;
+    private Comparator<Position> comparator = Comparator.comparing(Position::getX).thenComparing(Position::getY);
+
+    public Polyomino(List<Position> squares) {
+        this.squares = squares.stream().sorted(comparator).collect(Collectors.toList());
+    }
+
+    public static Polyomino initPolyomino() {
+        return new Polyomino(Arrays.asList(new Position(0, 0)));
+    }
+
+    public Polyomino addOneSquare(final Position position) {
+        if (!isTouched(position)) {
+            throw new RuntimeException(String.format("position (%d,%d) can not be add to polyomino %s", position.getX(), position.getY(), this.toString()));
+        }
+        List<Position> squares = new ArrayList<>(this.squares);
+        squares.add(position);
+        squares.sort(comparator);
+        return new Polyomino(squares);
+    }
+
+    public boolean equalTo(Polyomino polyomino) {
+        return this.isTranslatedTo(polyomino) ||
+                this.isTranslatedTo(polyomino.rotate(90)) ||
+                this.isTranslatedTo(polyomino.rotate(180)) ||
+                this.isTranslatedTo(polyomino.rotate(270)) ||
+                this.isTranslatedTo(polyomino.reflect()) ||
+                this.isTranslatedTo(polyomino.reflect().rotate(90)) ||
+                this.isTranslatedTo(polyomino.reflect().rotate(180)) ||
+                this.isTranslatedTo(polyomino.reflect().rotate(270));
+    }
+
+    public Polyomino rotate(int degree) {
+        Function<Position, Position> rotate;
+        switch (degree) {
+            case 90: {
+                rotate = e -> rotate(e, new Position(0, 1));
+                break;
+            }
+            case 180: {
+                rotate = e -> rotate(e, new Position(-1, 0));
+                break;
+            }
+            case 270: {
+                rotate = e -> rotate(e, new Position(0, -1));
+                break;
+            }
+            default: {
+                throw new RuntimeException(String.format("Can not rotate with degree: %d, accepted degree are 90, 180, 270", degree));
+            }
+        }
+        return new Polyomino(this.getSquares().stream().map(rotate).sorted(comparator).collect(Collectors.toList()));
+    }
+
+    private static Position rotate(Position position, Position degree) {
+        int x = position.getX() * degree.getX() - position.getY() * degree.getY();
+        int y = position.getX() * degree.getY() + position.getY() * degree.getX();
+        return new Position(x, y);
+    }
+
+    public Polyomino reflect() {
+        return new Polyomino(this.getSquares().stream().map(Polyomino::reflect).sorted(comparator).collect(Collectors.toList()));
+    }
+
+    private static Position reflect(Position position) {
+        return new Position(position.getX(), -position.getY());
+    }
+
+    public List<Position> getPossiblePositions() {
+        return squares.stream()
+                .flatMap(Polyomino::getTouchedPositions)
+                .filter(e -> !isContained(e))
+                .distinct()
+                .collect(Collectors.toList());
+    }
+
+    private static Stream<Position> getTouchedPositions(Position center) {
+        int x = center.getX();
+        int y = center.getY();
+        return Stream.of(
+                new Position(x - 1, y),
+                new Position(x + 1, y),
+                new Position(x, y - 1),
+                new Position(x, y + 1));
+    }
+
+    private boolean isTouched(Position position) {
+        return squares.stream()
+                .anyMatch(e -> Math.abs(e.getY() - position.getY()) + Math.abs(e.getX() - position.getX()) == 1);
+    }
+
+    private boolean isContained(Position position) {
+        return squares.stream()
+                .anyMatch(e -> e.getX() == position.getX() && e.getY() == position.getY());
+    }
+
+    public boolean isTranslatedTo(final Polyomino polyomino) {
+        List<Position> targetSquares = polyomino.getSquares();
+        int x = targetSquares.get(0).getX() - squares.get(0).getX();
+        int y = targetSquares.get(0).getY() - squares.get(0).getY();
+        Position dir = new Position(x, y);
+        for (int i = 0; i < squares.size(); i++) {
+            if (!isTranslatedTo(squares.get(i), targetSquares.get(i), dir)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isTranslatedTo(Position rsc, Position dst, Position dir) {
+        return rsc.getX() + dir.getX() == dst.getX() && rsc.getY() + dir.getY() == dst.getY();
+    }
+
+    public List<Position> getSquares() {
+        return squares;
+    }
+
+    @Override
+    public String toString() {
+        return "Polyomino{" +
+                "squares=" + squares +
+                '}';
+    }
+}
---
+++ src/test/java/PolyominoTest.java
@@ -0,0 +1,71 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by yifang on 5/8/2018.
+ */
+
+public class PolyominoTest {
+
+    private Polyomino polyomino;
+    private Polyomino threePolyomino1;
+
+    @Before
+    public void setUp() throws Exception {
+        polyomino = Polyomino.initPolyomino();
+        List<Position> positions = Arrays.asList(new Position(0, -1), new Position(0, 0), new Position(1, 0));
+        threePolyomino1 = new Polyomino(positions);
+    }
+
+    @Test
+    public void addOneSquare() throws Exception {
+        Position position = new Position(0, 1);
+        System.out.println(polyomino);
+        System.out.println(polyomino.addOneSquare(position));
+        System.out.println(polyomino);
+    }
+
+    @Test
+    public void rotate() throws Exception {
+        System.out.println(threePolyomino1);
+        System.out.println(threePolyomino1.rotate(90));
+        System.out.println(threePolyomino1.rotate(180));
+        System.out.println(threePolyomino1.rotate(270));
+        assertTrue(threePolyomino1.equalTo(threePolyomino1.rotate(90)));
+        assertTrue(threePolyomino1.equalTo(threePolyomino1.rotate(180)));
+        assertTrue(threePolyomino1.equalTo(threePolyomino1.rotate(270)));
+    }
+
+    @Test
+    public void reflact() throws Exception {
+        System.out.println(threePolyomino1);
+        System.out.println(threePolyomino1.reflect());
+        assertTrue(threePolyomino1.equalTo(threePolyomino1.reflect()));
+    }
+
+    @Test
+    public void getPossiblePositions() throws Exception {
+        System.out.println(polyomino);
+        System.out.println("possible positions: " + polyomino.getPossiblePositions());
+        Position position = new Position(0, 1);
+        System.out.println(polyomino.addOneSquare(position));
+        System.out.println("possible positions: " + polyomino.addOneSquare(position).getPossiblePositions());
+    }
+
+    @Test
+    public void isTranslatedTo() throws Exception {
+        Polyomino rsc = threePolyomino1;
+        List<Position> dstPositions = threePolyomino1.getSquares().stream().map(position -> new Position(position.getX() + 1, position.getY() - 1)).collect(Collectors.toList());
+        Polyomino dst = new Polyomino(dstPositions);
+        System.out.println(rsc);
+        System.out.println(dst);
+        assertTrue(rsc.isTranslatedTo(dst));
+    }
+
+}
